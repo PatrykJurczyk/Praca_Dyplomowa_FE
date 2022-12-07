a9d9e8fe2b05db261e1c7c1ae93d5608461e80fc
PatrykJurczyk <jurczykpatryk8@gmail.com>
2022-12-07 19:04:06 +0100

    user

--- src/app/pages/user-panel-page/user-houses/user-houses.component.ts
+++ src/app/pages/user-panel-page/user-houses/user-houses.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { HouseService } from '../../../services/house.service';
+import { Subject, takeUntil } from 'rxjs';
+import { HouseModel } from '../../../models/houseModel';
+import { isReserved, UserStorage } from '../../../enums/enum';
 
 @Component({
   selector: 'app-user-houses',
   templateUrl: './user-houses.component.html',
   styleUrls: ['./user-houses.component.scss'],
 })
-export class UserHousesComponent {}
+export class UserHousesComponent implements OnDestroy {
+  protected myHouses!: HouseModel[];
+  protected archivedHouses!: HouseModel[];
+
+  private destroy$: Subject<void> = new Subject();
+  constructor(private houseService: HouseService) {
+    this.getHouses();
+    this.houseService.Refreshrequired.pipe(takeUntil(this.destroy$)).subscribe(
+      () => {
+        this.getHouses();
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+  getHouses() {
+    this.houseService
+      .getHouses()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((houses: HouseModel[]) => {
+        this.myHouses = houses.filter(
+          (house: HouseModel) =>
+            house.owner === window.sessionStorage.getItem(UserStorage.USER_KEY)
+        );
+
+        this.archivedHouses = houses.filter(
+          (house: HouseModel) =>
+            house.owner ===
+              window.sessionStorage.getItem(UserStorage.USER_KEY) &&
+            house.isExist === isReserved.archiwizowany
+        );
+        console.log(this.myHouses);
+        console.log(this.archivedHouses);
+      });
+  }
+}
